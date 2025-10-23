9071352b6bd20bea3b38de08f686b809e7160205
Alif Yasa <130131893+alifyasa@users.noreply.github.com>
2025-10-23 03:32:37 +0000

    feat: Reset

--- jupyter_scheduler_keepalive/_version.py
+++
@@ -1,7 +0,0 @@
-import json
-from pathlib import Path
-
-__all__ = ["__version__"]
-
-version_info = (2, 10, 0, "", "")
-__version__ = ".".join(map(str, version_info[:3])) + "".join(version_info[3:])
--- jupyter_scheduler_keepalive/pydantic_v1/__init__.py
+++
@@ -1,8 +0,0 @@
-from importlib import metadata
-
-# expose Pydantic v1 API, regardless of Pydantic version in current env
-
-try:
-    from pydantic.v1 import *
-except ImportError:
-    from pydantic import *
